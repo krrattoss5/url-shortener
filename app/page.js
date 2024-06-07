890f59e1354dedd15bf7fea5ddcf41ae0290880c
krrattoss5 <krrattoss5@gmail.com>
2024-06-06 23:38:01 -0500

    (add/CreateShortURL.jsx)

--- app/page.js
+++ app/page.js
@@ -1,35 +1,11 @@
-import { useRef, useState } from "react";
-import styles from "./page.module.css";
+"use client"
+import CreateShortURL from "./components/createShortURL/CreateShortURL.jsx";
 
 export default function Home() {
-  const inputRef = useRef()
-  const [shortURL, setShortURL] = useState("")
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const url = inputRef.current.value
-    // ToDo: peticion al API
-  }
 
   return (
     <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>URL Shortener</h1>
-
-        <p className={styles.description}>
-          Acorta tus URLs aquí!
-        </p>
-
-        <div className={styles.grid}>
-          <form className={styles.card}>
-            <input ref={inputRef} type="text" className={styles.input} placeholder='URL' onSubmit={handleSubmit} />
-            <button className={styles.button}>Acorta</button>
-            <span className={styles.input}>
-              {shortURL}
-            </span>
-          </form>
-        </div>
-      </main>
+      <CreateShortURL />
     </div>
   );
 }
