9aab7ffe1bcdf2e183cdd727b07f61fd5de4d189
krrattoss5 <krrattoss5@gmail.com>
2024-06-07 23:59:48 -0500

    add/feature redirect)

--- src/components/singleShort/SingleShort.tsx
+++ src/components/singleShort/SingleShort.tsx
@@ -1,17 +1,28 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 
 const SingleShort = () => {
   const shortId = useLocation().pathname
+  const [url, setURL] = useState<string | null>('')
 
   useEffect(() => {
-    fetch(`http://localhost:3001/${shortId}`)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    fetch(`http://localhost:3001${shortId}`)
+      .then(res => res.json())
+      .then(data => setURL(data))
+      .catch((error) => console.log({message: error.message}))
 
+      url
+        ? window.location.href = url
+        : null
+
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
   return (
     <div>
+      {url
+        ? window.location.href = url
+        : null}
     </div>
   )
 }
