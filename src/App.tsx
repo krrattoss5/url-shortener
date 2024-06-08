02abdf61112c9b05491f61a66b9958351067868c
krrattoss5 <krrattoss5@gmail.com>
2024-06-08 15:25:20 -0500

    (add/ Routes)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import CreateShortURL from './components/createShortURL/CreateShortURL'
 import SingleShort from './components/singleShort/SingleShort';
+import Login from './components/login/Login.tsx';
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
     <div>
       <Routes>
         <Route path='/' element={<CreateShortURL />} />
-        <Route path='/:id' element={<SingleShort />} />
+        <Route path='/crear-usuario' element={<Login />} />
+        <Route path='/:shortId' element={<SingleShort />} />
       </Routes>
     </div>
   );
