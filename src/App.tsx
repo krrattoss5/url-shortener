4e49ea8e7991bd831745f6db0b4d87ab639f6c25
krrattoss5 <krrattoss5@gmail.com>
2024-06-09 15:25:38 -0500

    (add/redirection by athorization)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css'
 // import CreateShortURL from './components/createShortURL/CreateShortURL'
-import SingleShort from './components/singleShort/SingleShort';
 import Register from './components/register/Register.tsx';
 import Login from './components/login/Login.tsx';
 import NavBar from './components/navBar/NavBar.tsx';
 import Landing from './components/landing/Landing.tsx';
 import Footer from './components/footer/Footer.tsx';
+import ProtectedRoute from './components/verifyAuth/ProtectedRoute.tsx';
+import Dashboard from './components/dashboard/Dashboard.tsx';
+import PublicRoute from './components/verifyAuth/PublicRoute.tsx';
 
 function App() {
+  const {pathname} = useLocation()
+  const showBar = pathname !== '/iniciar-sesion' && pathname !== '/crear-usuario'
+
+
 
   return (
     <div>
-      <NavBar />
+      {showBar && <NavBar />}
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/crear-usuario' element={<Register />} />
-        <Route path='/iniciar-sesion' element={<Login />} />
-        <Route path='/:shortId' element={<SingleShort />} />
+        <Route path='/' element={<PublicRoute element={<Landing />} />} />
+        <Route path='/crear-usuario' element={<PublicRoute element={<Register />} />} />
+        <Route path='/iniciar-sesion' element={<PublicRoute element={<Login />} />} />
+        <Route path='/dashboard' element={<ProtectedRoute element={<Dashboard />} />} />
       </Routes>
       <Footer />
     </div>
