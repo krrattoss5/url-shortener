87a750b59b193bafd67b60d0757c2635fbd469fb
krrattoss5 <krrattoss5@gmail.com>
2024-06-09 15:09:29 -0500

    (add/ProtectedRout.tsx)

---
+++ src/components/verifyAuth/ProtectedRoute.tsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom"
+import { isAuthenticated } from "../../constants"
+
+interface Props {
+  element: JSX.Element
+}
+
+const ProtectedRoute = ({element}: Props) => {
+  console.log()
+  return isAuthenticated()
+    ? element
+    : <Navigate to='/iniciar-sesion' />
+}
+
+export default ProtectedRoute
