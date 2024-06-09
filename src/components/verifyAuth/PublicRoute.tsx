1d89c6d6dac85c70451a1a6b76524ef3a401a83f
krrattoss5 <krrattoss5@gmail.com>
2024-06-09 15:09:54 -0500

    (add/PublicRoute.tsx)

---
+++ src/components/verifyAuth/PublicRoute.tsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom"
+import { isAuthenticated } from "../../constants"
+
+interface Props {
+  element: JSX.Element
+}
+
+const PublicRoute = ({element}: Props) => {
+  return isAuthenticated()
+   ? <Navigate to='/dashboard' />
+   : element
+}
+
+export default PublicRoute
