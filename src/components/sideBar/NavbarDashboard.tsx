3b75719ddfa92c8fd3baf1750645f880418f4ad4
krrattoss5 <krrattoss5@gmail.com>
2024-06-10 19:07:05 -0500

    (add/NavbarDashboard.tsx)

---
+++ src/components/sideBar/NavbarDashboard.tsx
@@ -0,0 +1,11 @@
+import s from './NabarDashboard.module.css'
+
+const NavbarDashboard = () => {
+  return (
+    <header className={s.container}>
+      <h1>cabecera</h1>
+    </header>
+  )
+}
+
+export default NavbarDashboard
