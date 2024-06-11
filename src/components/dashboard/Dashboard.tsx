3997e4e49b8bcda892cd08337b9e2c518b282bb0
krrattoss5 <krrattoss5@gmail.com>
2024-06-10 19:23:17 -0500

    (add/routes and lazy load components)

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,10 +1,31 @@
+import { Route, Routes } from 'react-router-dom'
+import {Suspense, lazy} from 'react'
+import NavbarDashboard from '../sideBar/NavbarDashboard'
 import SideBar from '../sideBar/SideBar'
 import s from './Dashboard.module.css'
+const Home = lazy(() => import('./Home.tsx'))
+const Links = lazy(() => import('./Links.tsx'))
+const Settings = lazy(() => import('./Settings.tsx'))
 
 const Dashboard = () => {
   return (
     <main className={s.main}>
       <SideBar />
+
+      <section className={s.routeSection}>
+        <NavbarDashboard />
+        <div className={s.component}>
+          <Suspense fallback={
+            <h1>Loading...</h1>
+          }>
+            <Routes>
+              <Route path='/home' element={<Home />} />
+              <Route path='/links' element={<Links />} />
+              <Route path='/settings' element={<Settings />} />
+            </Routes>
+          </Suspense>
+        </div>
+      </section>
     </main>
   )
 }
