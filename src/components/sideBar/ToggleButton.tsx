5b21375069f5a5611b0069c3ce53683a61467f70
krrattoss5 <krrattoss5@gmail.com>
2024-06-10 19:05:19 -0500

    (add/ToggleButton.tsx)

---
+++ src/components/sideBar/ToggleButton.tsx
@@ -0,0 +1,17 @@
+import { MinusTo, PlusToIcon } from "../icons";
+import s from './SideBar.module.css';
+
+interface Props {
+  isToggle: boolean
+  handleIsToggle: () => void
+}
+
+const ToggleButton = ({ isToggle, handleIsToggle }: Props) => {
+  return (
+    <button onClick={handleIsToggle} className={isToggle ? s.buttonToggleOf : s.buttonToggleOn}>
+      {isToggle ? <PlusToIcon /> : <MinusTo />}
+    </button>
+  );
+};
+
+export default ToggleButton;
