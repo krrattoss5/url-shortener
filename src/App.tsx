78a0b1830d6b38253b6895b4da75a1c5be388fa1
krrattoss5 <krrattoss5@gmail.com>
2024-06-11 20:07:36 -0500

    (add/logit to footer)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
 import './App.css'
 // import CreateShortURL from './components/createShortURL/CreateShortURL'
 import Register from './components/register/Register.tsx';
@@ -6,9 +7,9 @@ import Login from './components/login/Login.tsx';
 import NavBar from './components/navBar/NavBar.tsx';
 import Landing from './components/landing/Landing.tsx';
 import Footer from './components/footer/Footer.tsx';
-import ProtectedRoute from './components/verifyAuth/ProtectedRoute.tsx';
+const ProtectedRoute = lazy(() => import('./components/verifyAuth/ProtectedRoute.tsx'))
+const PublicRoute = lazy(() => import('./components/verifyAuth/PublicRoute.tsx'))
 import Dashboard from './components/dashboard/Dashboard.tsx';
-import PublicRoute from './components/verifyAuth/PublicRoute.tsx';
 
 function App() {
   const {pathname} = useLocation()
@@ -16,19 +17,22 @@ function App() {
     pathname !== '/iniciar-sesion'
     && pathname !== '/crear-usuario'
     && pathname.startsWith('/dashboard'))
+  const showFooter = !pathname.startsWith('/dashboard')
 
 
 
   return (
     <div>
       {showBar && <NavBar />}
-      <Routes>
-        <Route path='/' element={<PublicRoute element={<Landing />} />} />
-        <Route path='/crear-usuario' element={<PublicRoute element={<Register />} />} />
-        <Route path='/iniciar-sesion' element={<PublicRoute element={<Login />} />} />
-        <Route path='/dashboard/*' element={<ProtectedRoute element={<Dashboard />} />} />
-      </Routes>
-      <Footer />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<PublicRoute element={<Landing />} />} />
+          <Route path='/crear-usuario' element={<PublicRoute element={<Register />} />} />
+          <Route path='/iniciar-sesion' element={<PublicRoute element={<Login />} />} />
+          <Route path='/dashboard/*' element={<ProtectedRoute element={<Dashboard />} />} />
+        </Routes>
+      </Suspense>
+      {showFooter && <Footer />}
     </div>
   );
 }
