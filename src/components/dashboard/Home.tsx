62e4512e2eaf4a97280c0ad41c0cb826e4f245b2
krrattoss5 <krrattoss5@gmail.com>
2024-06-11 20:10:30 -0500

    (add/Home basic structure)

--- src/components/dashboard/Home.tsx
+++ src/components/dashboard/Home.tsx
@@ -1,7 +1,36 @@
+import s from './Dashboard.module.css'
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={s.containerSection}>
+
+      <div className={s.containerSectionHead}>
+
+        <h2>
+          Your Clone Connections Platform
+        </h2>
+
+      </div>
+
+      <div className={s.containerSectionBox}>
+
+        <div className={s.sectionBox}>
+          <img src="/dashboard-custom-domain.png" alt="" />
+        </div>
+
+        <div className={s.sectionBox}>
+
+          <img src="/dashboard-custom-domain.png" alt="" />
+
+          <h3>Replace your brand with “bit.ly clone”.</h3>
+
+          <p>Get a custom domain to create links that represent you. Add your own short domain or choose a complimentary one when you upgrade.</p>
+
+          <button>new short</button>
+        </div>
+
+      </div>
+
     </div>
   )
 }
