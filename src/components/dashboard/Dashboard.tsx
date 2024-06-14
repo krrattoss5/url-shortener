a73df22ffdb1bce602360656cc7ae43a753b815a
krrattoss5 <krrattoss5@gmail.com>
2024-06-13 21:53:27 -0500

    (add/useUser: give the correct props to the correct components with your respective types )

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -4,17 +4,20 @@ import NavbarDashboard from '../sideBar/NavbarDashboard'
 import SideBar from '../sideBar/SideBar'
 import s from './Dashboard.module.css'
 import CreateShortURL from '../createShortURL/CreateShortURL.tsx'
+import useUsers from '../../hooks/useUsers.tsx'
 const Home = lazy(() => import('./Home.tsx'))
 const Links = lazy(() => import('../links/Links.tsx'))
 const Settings = lazy(() => import('./Settings.tsx'))
 
 const Dashboard = () => {
+  const {links, user} = useUsers()
+
   return (
     <main className={s.main}>
       <SideBar />
 
       <section className={s.routeSection}>
-        <NavbarDashboard />
+        <NavbarDashboard user={user} />
         <div className={s.component}>
           <Suspense fallback={
             <h1>Loading...</h1>
@@ -22,7 +25,7 @@ const Dashboard = () => {
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/home' element={<Home />} />
-              <Route path='/links' element={<Links />} />
+              <Route path='/links' element={<Links links={links} />} />
               <Route path='/settings' element={<Settings />} />
               <Route path='/links/create' element={<CreateShortURL />} />
             </Routes>
