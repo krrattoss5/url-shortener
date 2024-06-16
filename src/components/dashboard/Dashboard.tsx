a3a8fd5d642301a57e066bba67263c236eee7aea
krrattoss5 <krrattoss5@gmail.com>
2024-06-15 22:45:25 -0500

    (add route: /links/update/:id and component UpdateShortURL )

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -3,13 +3,14 @@ import {Suspense, lazy} from 'react'
 import NavbarDashboard from './navbarDashboard/NavbarDashboard.tsx'
 import SideBar from './sideBar/SideBar.tsx'
 import s from './Dashboard.module.css'
-import CreateShortURL from './createShortURL/CreateShortURL.tsx'
+import CreateShortURL from './shortURL/CreateShortURL.tsx'
 import useUsers from '../../hooks/useUsers.tsx'
 import SingleLink from './links/SingleLink.tsx'
 import Loading from '../Loading.tsx'
+import UpdateShortURL from './shortURL/UpdateShortURL.tsx'
 const Home = lazy(() => import('./Home.tsx'))
 const Links = lazy(() => import('./links/Links.tsx'))
-const Settings = lazy(() => import('./Settings.tsx'))
+const Settings = lazy(() => import('./settings/Settings.tsx'))
 
 const Dashboard = () => {
   const {links, user, countries} = useUsers()
@@ -29,8 +30,9 @@ const Dashboard = () => {
               <Route path='/' element={<Home />} />
               <Route path='/home' element={<Home />} />
               <Route path='/links' element={<Links links={links} />} />
-              <Route path='/settings' element={<Settings />} />
+              <Route path='/settings' element={<Settings user={user} />} />
               <Route path='/links/create' element={<CreateShortURL />} />
+              <Route path='/links/update/:id' element={<UpdateShortURL links={links} />} />
               <Route path='/links/single/:id' element={<SingleLink links={links} countries={countries} />} />
             </Routes>
           </Suspense>
