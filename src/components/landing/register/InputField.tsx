36beb529c0030e73d7bd98987e529f879cbdb03e
krrattoss5 <krrattoss5@gmail.com>
2024-06-18 17:09:40 -0500

    (add/InputField)

---
+++ src/components/landing/register/InputField.tsx
@@ -0,0 +1,26 @@
+interface Props {
+  label: string;
+  name: string;
+  value: string;
+  type: string;
+  placeholder: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const InputField = ({
+  label,
+  name,
+  value,
+  type,
+  placeholder,
+  onChange,
+}: Props) => {
+  return (
+    <>
+      <label>{label}</label>
+      <input name={name} value={value} placeholder={placeholder} type={type} onChange={onChange} />
+    </>
+  )
+};
+
+export default InputField;
