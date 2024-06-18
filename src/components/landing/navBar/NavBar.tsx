2fd8feb3e4da7be201440a5b5e1ef8e8fdcdd950
krrattoss5 <krrattoss5@gmail.com>
2024-06-18 14:49:11 -0500

    (refact navbar landing styles)

--- src/components/landing/navBar/NavBar.tsx
+++ src/components/landing/navBar/NavBar.tsx
@@ -1,32 +1,56 @@
-import { Link } from "react-router-dom"
-import s from './NavBar.module.css'
-import { getOut } from "../../../constants"
+import { Link } from "react-router-dom";
+import s from './NavBar.module.css';
+import { getOut } from "../../../constants";
+import { useState, useEffect } from "react";
 
 const NavBar = () => {
-  const isLogin = !!localStorage.getItem('token')
+  const isLogin = !!localStorage.getItem('token');
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (isScrolled) {
+      setTimeout(() => {
+        document.querySelector(`.${s.container}`)?.classList.add(s.transition);
+      }, 0);
+    } else {
+      document.querySelector(`.${s.container}`)?.classList.remove(s.transition);
+    }
+  }, [isScrolled]);
 
   return (
-    <nav className={s.container}>
+    <nav className={`${s.container} ${isScrolled ? s.scrolled : ''}`}>
       <Link to='/'>
         <img src="/bitly_logo.svg" alt="bitly clone" />
       </Link>
       <div>
-
         {!isLogin
           ? <Link className={s.link} to='/iniciar-sesion'>
               Log in
             </Link>
           : <button className={s.link} onClick={getOut}>Get Out</button>}
-
         {!isLogin
           ? <Link className={s.link2} to='/crear-usuario'>
-              Sigin up Free
+              Sign up Free
             </Link>
           : null
         }
       </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
