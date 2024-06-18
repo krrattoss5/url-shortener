f62752c57a3f011f474f2ae18aa91532b6046133
krrattoss5 <krrattoss5@gmail.com>
2024-06-18 18:27:10 -0500

    (add/ Loading if the links array dot length is less to 1)

--- src/components/dashboard/links/Links.tsx
+++ src/components/dashboard/links/Links.tsx
@@ -1,19 +1,21 @@
 import { type Urls } from "../../../Types";
 import Cards from "./Cards.tsx";
 import s from "../Dashboard.module.css";
+import Loading from "../../Loading.tsx";
 
 interface Props {
   links: Urls[] | null;
 }
 
 const Links = ({ links }: Props) => {
-  console.log(links);
   if (links && links?.length < 1) {
     links = null;
   }
   return (
     <div className={s.containerSection}>
-      {links ? (
+      {!links ? (
+        <Loading />
+      ) : links.length > 0 ? (
         <h1 className={s.title}>Bitly Clone Links</h1>
       ) : (
         <img src="/links-list-empty.png" alt="links-list-empty" />
