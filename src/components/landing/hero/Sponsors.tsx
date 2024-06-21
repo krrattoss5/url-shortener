bf4fd2ec707104ee65154c15cc958d01ff9c233c
krrattoss5 <krrattoss5@gmail.com>
2024-06-21 17:18:28 -0500

    (add/new component than renderized all sponsors logos)

---
+++ src/components/landing/hero/Sponsors.tsx
@@ -0,0 +1,15 @@
+import s from './Hero.module.css'
+
+const Sponsors = () => {
+  return (
+    <div className={s.section3}>
+      <img src="/curology-2.svg" alt="" />
+      <img src="/novasol.svg" alt="" />
+      <img src="/rad-bikes-2.svg" alt="" />
+      <img src="/marriott.svg" alt="" />
+      <img src="/new-york-times.svg" alt="" />
+    </div>
+  );
+};
+
+export default Sponsors;
