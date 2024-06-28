d9e44283fc89b65a4b92721a7697156e158ffde4
krrattoss5 <krrattoss5@gmail.com>
2024-06-27 22:24:32 -0500

    (add responsive styles to NavigationLink.tsx)

--- src/components/dashboard/sideBar/NavigationLink.tsx
+++ src/components/dashboard/sideBar/NavigationLink.tsx
@@ -1,26 +1,30 @@
 import { NavLink } from "react-router-dom";
-import s from './SideBar.module.css';
+import s from "./SideBar.module.css";
 import { ReactNode } from "react";
 
 interface Props {
-  to: string
-  icon: ReactNode
-  label: string
-  isToggle: boolean
+  to: string;
+  icon: ReactNode;
+  label: string;
+  isToggle: boolean;
+  handleToggle: ()=>void
 }
 
-const NavigationLink = ({ to, icon, label, isToggle }: Props) => {
+const NavigationLink = ({ to, icon, label, isToggle, handleToggle = ()=>{} }: Props) => {
   return (
-    <NavLink to={to} className={({ isActive }) => {
-      if(isToggle) return isActive
-        ? `${s.noToggleActive} ${s.selected}`
-        : `${s.noToggle} ${s.selected}`
+    <NavLink
+      to={to}
+      key={label}
+      className={({ isActive }) => {
+        if (isToggle)
+          return isActive
+            ? `${s.noToggleActive} ${s.selected}`
+            : `${s.noToggle} ${s.selected}`;
 
-      if(!isToggle) return isActive
-        ? s.noToggleActive
-        : s.noToggle
-
-    }}>
+        if (!isToggle) return isActive ? s.noToggleActive : s.noToggle;
+      }}
+      onClick={handleToggle}
+    >
       <span>|</span>
       {icon}
       {!isToggle && <h4>{label}</h4>}
