6c032e41c9152695027ede647528ba4063d7e4b6
krrattoss5 <krrattoss5@gmail.com>
2024-06-27 22:23:54 -0500

    (add responsive styles to sidebar.tsx)

--- src/components/dashboard/sideBar/SideBar.tsx
+++ src/components/dashboard/sideBar/SideBar.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { ConfigIcon, HomeIcon, LinkIcon, PlusIcon } from "../../icons.tsx";
-import s from './SideBar.module.css';
-import NavigationLink from './NavigationLink.tsx';
-import ToggleButton from './ToggleButton.tsx';
+import s from "./SideBar.module.css";
+import NavigationLink from "./NavigationLink.tsx";
+import ToggleButton from "./ToggleButton.tsx";
 import { Link } from "react-router-dom";
 
 const NAV_LINKS = [
-  { to: '/dashboard/home', icon: <HomeIcon />, label: 'Home' },
-  { to: '/dashboard/links', icon: <LinkIcon />, label: 'Links' },
-  { to: '/dashboard/settings', icon: <ConfigIcon />, label: 'Settings' }
+  { to: "/dashboard/home", icon: <HomeIcon />, label: "Home" },
+  { to: "/dashboard/links", icon: <LinkIcon />, label: "Links" },
+  { to: "/dashboard/settings", icon: <ConfigIcon />, label: "Settings" },
 ];
 
 const SideBar = () => {
@@ -17,21 +17,23 @@ const SideBar = () => {
   const handleIsToggle = () => setIsToggle(!isToggle);
 
   return (
-    <div className={`${s.container} ${isToggle ? s.selected : ''}`}>
-      <section className={`${s.ContainerToggle} ${isToggle ? s.selected : ''}`}>
+    <div className={`${s.container} ${isToggle ? s.selected : ""}`}>
+      <section className={`${s.ContainerToggle} ${isToggle ? s.selected : ""}`}>
         <ToggleButton isToggle={isToggle} handleIsToggle={handleIsToggle} />
 
-        <Link to='/'>
+        <Link to="/dashboard/home">
           <img src="/favicon.ico" alt="favicon" className={s.favicon} />
         </Link>
 
-        <Link to='/dashboard/links/create'>
-          <button className={`${s.buttonOnToggle} ${isToggle ? s.selected : ''}`}>
-            {isToggle ? <PlusIcon /> : 'Create new'}
+        <Link to="/dashboard/links/create">
+          <button
+            className={`${s.buttonOnToggle} ${isToggle ? s.selected : ""}`}
+          >
+            {isToggle ? <PlusIcon /> : "Create new"}
           </button>
         </Link>
 
-        {NAV_LINKS.map(link => (
+        {NAV_LINKS.map((link) => (
           <NavigationLink
             key={link.to}
             to={link.to}
@@ -40,7 +42,6 @@ const SideBar = () => {
             isToggle={isToggle}
           />
         ))}
-
       </section>
     </div>
   );
